84af735b3b5e10e5a90d4009d640c5c9c6b13441
Gael-Morales-Monje <gaelmoralesmonje@gmail.com>
2025-01-10 18:41:31 -0600

    se mejoro el estilo del print

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const inputUMA = $('#inputUMA')
 const checkboxSW = $('.checkboxSW')
 const checkboxUMA = $('.checkboxUMA')
 
-
 const CHECKS = (check,input) => {
   check.addEventListener('change', () => {
     if (check.checked) {
@@ -23,6 +22,14 @@ const CHECKS = (check,input) => {
   })
 }
 
+
+const renderRows = (element,name,inner) => {
+  element.innerHTML += `
+  <tr>
+    <td class='name'>${name}</td>
+    <td>${inner}</td>
+  </tr>`
+}
 
 
 CHECKS(checkboxSW,inputSW)
@@ -35,19 +42,37 @@ const info = $('.info')
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault()
   
-  info.innerHTML = ''
+  
+  info.innerHTML = `<table>
+          <thead>
+            <tr>
+              <th colspan="3"> Tabla de Valores</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            
+          
+          </tbody>
+        </table>`
 
+  const tbody = $('#tbody')
+  let innerCode = ""
+
+  renderRows(tbody,'Nombre',$('#name').value)
+  renderRows(tbody,'Meses trabajados',months.value)
+  renderRows(tbody,'Dias trabajados',days.value)
+  renderRows(tbody,'Salario minimo',`${inputSW.value}$`)
+  renderRows(tbody,'UMA',inputUMA.value)
+  
 
   const bonusPerMonth = 15/12
-  let bonusMonth = bonusPerMonth * months.value
-  let bonusDay = (bonusPerMonth/30) * days.value
+  let bonusMonth = bonusPerMonth.toFixed(3) * months.value
+  let bonusDay = (bonusPerMonth.toFixed(3)/30) * days.value
 
   let bonus = bonusMonth + bonusDay
-
-  
   const bonusTotal = bonus * inputSW.value
-
-  info.innerHTML += `<div class='infoContainer'>${bonus} dias x ${inputSW.value}$ = ${bonusTotal}$ de aguinaldo</div>`
+  
+  renderRows(tbody,'Aguinaldo',`${bonus} dias x ${inputSW.value}$ = ${bonusTotal.toFixed(3)}$`)
   
   const proporcion = 30/12
   
@@ -55,12 +80,11 @@ document.getElementById('form').addEventListener('submit', (e) => {
   let propDay = proporcion/30 * days.value
   
   const UMATotal = inputUMA.value * (propMonth+propDay)
-  
-  info.innerHTML += `<div class='infoContainer'>${(propMonth+propDay)} x ${inputUMA.value}$ = ${UMATotal}$ propporcion del UMA</div>`
-  
-  
+
+
+  renderRows(tbody,'Cantidad exenta de inpuestos',`${(propMonth+propDay)} x ${inputUMA.value}$ = ${UMATotal}$`)
+
   const TOTAL = bonusTotal - UMATotal 
-  
-  info.innerHTML += `<div class='infoContainer'>${bonusTotal}$ - ${UMATotal}$ = ${TOTAL}$ la diferencia</div>`
+  renderRows(tbody,'Cantidad no exenta',`${bonusTotal}$ - ${UMATotal}$ = ${TOTAL.toFixed(3)}$`)
 
 }) 
